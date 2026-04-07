caf835948c58f63e41c6fed89a04ec3e37c93e77
potcover <bo.dou626@gmail.com>
2026-04-08 00:17:51 +0800

    46 permute

--- hot/medium/medium.go
+++ hot/medium/medium.go
@@ -55,7 +55,7 @@ func levelOrder(root *TreeNode) [][]int {
 
 }
 
-// 验证二叉搜索树
+// 98 验证二叉搜索树
 func isValidBST(root *TreeNode) bool {
 
 	// 中序遍历
@@ -83,4 +83,41 @@ func isValidBST(root *TreeNode) bool {
 	}
 
 	return dfs(root)
+}
+
+func permute(nums []int) [][]int {
+	res := make([][]int, 0)
+
+	used := make([]bool, len(nums))
+
+	var backtrace func(nums []int, used []bool, tmp []int, res *[][]int)
+
+	backtrace = func(nums []int, used []bool, tmp []int, res *[][]int) {
+		if len(nums) == len(tmp) {
+			t := make([]int, len(tmp))
+			copy(t, tmp)
+			*res = append(*res, t)
+			return
+		}
+
+		for i := 0; i < len(nums); i++ {
+			if used[i] {
+				continue
+			}
+			// 做选择
+			tmp = append(tmp, nums[i])
+			used[i] = true
+
+			// 回溯
+			backtrace(nums, used, tmp, res)
+
+			// 撤销选择
+			tmp = tmp[:len(tmp)-1]
+			used[i] = false
+		}
+	}
+
+	backtrace(nums, used, []int{}, &res)
+
+	return res
 }
---
+++ hot/medium/medium_test.go
@@ -0,0 +1,16 @@
+package medium
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPermute(t *testing.T) {
+	nums := []int{1, 2, 3}
+
+	res := permute(nums)
+
+	for _, data := range res {
+		fmt.Println(data)
+	}
+}
