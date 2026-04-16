208554a5683e7b3ea6fcd8c373250d6664d06ed9
potcover <bo.dou626@gmail.com>
2026-04-16 23:29:16 +0800

    322. 零钱兑换

--- medium/medium.go
+++ medium/medium.go
@@ -1,1 +1,26 @@
 package medium
+
+import "math"
+
+// 322. 零钱兑换
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 1; i <= amount; i++ {
+		dp[i] = math.MaxInt32
+	}
+
+	for i := 0; i <= amount; i++ {
+		for _, coin := range coins {
+			if i-coin < 0 {
+				continue
+			}
+			dp[i] = min(dp[i], dp[i-coin]+1)
+		}
+	}
+
+	if dp[amount] == math.MaxInt32 {
+		return -1
+	}
+
+	return dp[amount]
+}
--- medium/medium_test.go
+++ medium/medium_test.go
@@ -1,1 +1,12 @@
 package medium
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCoin(t *testing.T) {
+	coins := []int{1, 2, 5}
+
+	fmt.Println(coinChange(coins, 11))
+}
