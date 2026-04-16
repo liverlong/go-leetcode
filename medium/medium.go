0c5252f32a11b0156baf2efe304e6a3b9739c326
potcover <bo.dou626@gmail.com>
2026-04-16 23:52:30 +0800

    931. 下降路径最小和

--- medium/medium.go
+++ medium/medium.go
@@ -24,3 +24,38 @@ func coinChange(coins []int, amount int) int {
 
 	return dp[amount]
 }
+
+// 931. 下降路径最小和
+func minFallingPathSum(matrix [][]int) int {
+	n := len(matrix)
+
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	copy(dp[0], matrix[0])
+
+	// dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i-1][j+1]) + matrix[i][j]
+	for i := 1; i < n; i++ {
+		for j := 0; j < n; j++ {
+			minVal := dp[i-1][j]
+			if j > 0 {
+				minVal = min(minVal, dp[i-1][j-1])
+			}
+			if j+1 < n {
+				minVal = min(minVal, dp[i-1][j+1])
+			}
+
+			dp[i][j] = minVal + matrix[i][j]
+		}
+	}
+
+	minRes := dp[n-1][0]
+
+	for i := 1; i < n; i++ {
+		minRes = min(minRes, dp[n-1][i])
+	}
+
+	return minRes
+}
