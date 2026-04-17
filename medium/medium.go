210ff23881876e2062e4a683ddb56d941375762a
potcover <bo.dou626@gmail.com>
2026-04-17 23:31:56 +0800

    72 编辑距离

--- medium/medium.go
+++ medium/medium.go
@@ -59,3 +59,54 @@ func minFallingPathSum(matrix [][]int) int {
 
 	return minRes
 }
+
+// 72. 编辑距离
+func minDistance(word1 string, word2 string) int {
+	//m, n := len(word1), len(word2)
+	//
+	//var dp func(word1 string, m int, word2 string, n int) int
+	//
+	//dp = func(word1 string, m int, word2 string, n int) int {
+	//	if m == -1 {
+	//		return n + 1
+	//	}
+	//
+	//	if n == -1 {
+	//		return m + 1
+	//	}
+	//
+	//	if word1[m] == word2[n] {
+	//		return dp(word1, m-1, word2, n-1)
+	//	}
+	//
+	//	return min(dp(word1, m-1, word2, n-1), dp(word1, m-1, word2, n), dp(word1, m, word2, n-1)) + 1
+	//
+	//}
+	//
+	//return dp(word1, m-1, word2, n-1)
+
+	m, n := len(word1), len(word2)
+
+	dp := make([][]int, m+1)
+
+	for i := 0; i < m+1; i++ {
+		dp[i] = make([]int, n+1)
+		dp[i][0] = i
+	}
+
+	for i := 0; i < n+1; i++ {
+		dp[0][i] = i
+	}
+
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < n+1; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
+			}
+		}
+	}
+
+	return dp[m][n]
+}
