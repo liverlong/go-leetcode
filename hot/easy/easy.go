1e066ce8b737209f63d2e622adffbd85fca823ff
potcover <bo.dou626@gmail.com>
2026-04-18 22:32:11 +0800

    617. 合并二叉树

--- hot/easy/easy.go
+++ hot/easy/easy.go
@@ -101,3 +101,19 @@ func climbStairs(n int) int {
 
 	return res
 }
+
+// 617. 合并二叉树
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil || root2 == nil {
+		if root1 == nil {
+			return root2
+		}
+		return root1
+	}
+	root1.Val += root2.Val
+	left := mergeTrees(root1.Left, root2.Left)
+	right := mergeTrees(root1.Right, root2.Right)
+	root1.Left, root1.Right = left, right
+
+	return root1
+}
