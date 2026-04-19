545b82c4b42a9cb95e85a8b406953700b1c72497
potcover <bo.dou626@gmail.com>
2026-04-19 22:55:41 +0800

    // 338. 比特位计数

--- hot/easy/easy.go
+++ hot/easy/easy.go
@@ -174,3 +174,19 @@ func reverseList(head *ListNode) *ListNode {
 
 	return pre
 }
+
+// 338. 比特位计数
+func countBits(n int) []int {
+	res := make([]int, n+1)
+	res[0] = 0
+
+	for i := 0; i <= n; i++ {
+		if i%2 == 0 { // 偶数
+			res[i] = res[i%2]
+		} else {
+			res[i] = res[i-1] + 1
+		}
+	}
+
+	return res
+}
